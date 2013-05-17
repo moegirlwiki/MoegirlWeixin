fe95eea24e75c00390561c174c9f7f91a81c0c1b
xuechong <xuechong87@gmail.com>
2013-05-17 12:37:52 +0800

    add interfaces

---
+++ src/main/java/org/moegirlwiki/plugins/messagerobot/utils/feed/FeedGetter.java
@@ -0,0 +1,13 @@
+package org.moegirlwiki.plugins.messagerobot.utils.feed;
+
+import org.moegirlwiki.plugins.messagerobot.interfaces.OriginDataGetter;
+import org.moegirlwiki.plugins.messagerobot.interfaces.OriginData;
+
+public class FeedGetter implements OriginDataGetter{
+
+	@Override
+	public OriginData getOriginData() {
+		return null;
+	}
+
+}
