80c7ba1d676b76b10cf829932314a0a9b55bdf8b
xuechong87 <xuechong87@gmail.com>
2013-05-17 20:46:24 +0800

    add filter

--- src/main/java/org/moegirlwiki/plugins/messagerobot/interfaces/Push.java
+++ src/main/java/org/moegirlwiki/plugins/messagerobot/interfaces/Push.java
@@ -1,9 +1,12 @@
 package org.moegirlwiki.plugins.messagerobot.interfaces;
 
+import java.util.Collection;
+
 /**
  * the behavior of all pusher
  * @author xuechong
  */
 public interface Push {
 	public Object push(Message msg);
+	public Object push(Collection<Message> msgs);
 }
--- src/main/java/org/moegirlwiki/plugins/messagerobot/interfaces/Translator.java
+++ src/main/java/org/moegirlwiki/plugins/messagerobot/interfaces/Translator.java
@@ -1,5 +1,9 @@
 package org.moegirlwiki.plugins.messagerobot.interfaces;
 
+import java.util.Collection;
+
 public interface Translator {
 	public Message translate(OriginData origin);
+	
+	public Collection<Message> translate(Collection<OriginData> origin);
 }
